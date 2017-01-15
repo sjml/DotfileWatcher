f5711fe7ef56be67d58ea37b851e7dacee294563
Shane Liesegang <shane@techie.net>
2017-01-15 15:23:31 -0800

    initial commit of basic functionality; only watches .dotfiles and openly opens in Code. could make it configurable, but don't care enough at the moment

--- DotfileWatcher/AppDelegate.swift
+++ DotfileWatcher/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  DotfileWatcher
-//
-//  Created by Liesegang, Shane on 1/15/17.
-//
-//
-
 import Cocoa
 
 @NSApplicationMain
@@ -13,15 +5,51 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    var statusItem: NSStatusItem? = nil
+    var darkModeOn: Bool = false
+    var timerCheck: Timer!
+    var dotPath: String = ""
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        self.statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        self.statusItem!.image = NSImage(named: "menu-bar.pdf")
+        self.statusItem!.button?.action = #selector(AppDelegate.indicatorClicked(_:))
+        self.statusItem!.isVisible = false
+        
+        self.dotPath = NSHomeDirectory().appending("/.dotfiles")
+        
+        checkDotFiles(nil)
+        timerCheck = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(checkDotFiles), userInfo: nil, repeats: true)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // nada
     }
 
+    func indicatorClicked(_ sender: AnyObject?) {
+        NSWorkspace.shared().openFile(self.dotPath, withApplication: "Visual Studio Code", andDeactivate: true)
+    }
+    
+    func checkDotFiles(_ sender: AnyObject?) {
+        let check = Process()
+        let output = Pipe()
+        check.currentDirectoryPath = self.dotPath
+        check.launchPath = "/usr/bin/env"
+        check.standardOutput = output
+        check.arguments = ["git", "status", "--porcelain"]
+        check.launch()
+        check.waitUntilExit()
+        
+        let out = output.fileHandleForReading.readDataToEndOfFile()
+        if (out.count > 0) {
+            // something's going on
+            self.statusItem?.isVisible = true
+        }
+        else {
+            self.statusItem?.isVisible = false
+        }
+    }
+    
 
 }
 

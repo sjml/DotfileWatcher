6d3778e240066b389840af982a8aaf417a4a6f01
Shane Liesegang <shane@techie.net>
2020-11-26 16:26:31 +0200

    release script

---
+++ release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BUILD_DIR=build
+APP_NAME=DotfileWatcher
+UPLOAD_ASSET=${BUILD_DIR}/${APP_NAME}.zip
+
+clean=$(git status --porcelain)
+if [[ ${#clean} -gt 0 ]]; then
+  echo "Project directory isn't clean."
+  exit 1
+fi
+
+if [[ ! -f ${UPLOAD_ASSET} ]]; then
+  echo "No file found at ${UPLOAD_ASSET}. Did you build?"
+  exit 1
+fi
+
+echo "Checking asset..."
+unzip ${UPLOAD_ASSET} -d ${BUILD_DIR}
+codesign -vvvv ${APP_NAME}.app
+if [[ $? -ne 0 ]]; then
+  echo "App isn't signed."
+  exit 1
+fi
+xcrn stapler validate ${APP_NAME}.app
+if [[ $? -ne 0 ]]; then
+  echo "App isn't validated."
+  exit 1
+fi
+
+
+LAST_RELEASE=$(hub release | head -1)
+
+read -p "New release tag? (Last one was ${LAST_RELEASE}.) " RELEASE_TAG
+
+echo "What should the release message be?"
+read -p "> " MESSAGE
+
+hub release create -a ${UPLOAD_ASSET} -m ${MESSAGE} ${RELEASE_TAG}
